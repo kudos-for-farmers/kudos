e5f1547416b5e18f18aebf086bb688f17c720a8c
webthethird <ratzbodell@gmail.com>
2022-03-19 16:51:42 -0500

    In mint check allocation >= mintedByGuild + amount

--- packages/hardhat/contracts/KudosToken.sol
+++ packages/hardhat/contracts/KudosToken.sol
@@ -13,9 +13,12 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 /// @dev A non-transferable rewards token for FoodDAO which is issued by Kudos Guilds.
 /// A Guild contract can only mint as many Kudos tokens as its balance of AllocationToken.
 /// Many Guilds use the same KudosToken and AllocationToken, so we must track how many Kudos
-/// have been minted by a given address and store a reference to the AllocationToken contract
+/// have been minted by a given address and store a reference to the AllocationToken contract.
 contract KudosToken is Initializable, ERC20Upgradeable, PausableUpgradeable, OwnableUpgradeable {
+    // Non-transferable Allocations from the DAO are minted to each Guild on a regular (or streaming) basis
     AllocationToken public allocation;
+    // Map Guild addresses to the total amount of Kudos minted by that Guild
+    mapping(address => uint256) public mintedByGuild;
 
     function initialize(string memory name, string memory symbol, address allocationToken) initializer public {
         allocation = AllocationToken(allocationToken);
@@ -32,7 +35,10 @@ contract KudosToken is Initializable, ERC20Upgradeable, PausableUpgradeable, Own
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
+        require(allocation.balanceOf(msg.sender) >= mintedByGuild[msg.sender] + amount, 
+                "This Guild does not have a sufficient allocation to mint the requested number of Kudos.");
+        mintedByGuild[msg.sender] += amount;
         _mint(to, amount);
     }
 
