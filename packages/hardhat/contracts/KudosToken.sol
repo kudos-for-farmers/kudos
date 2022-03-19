0c3553ad8065fa1441660274c7d657874165629e
webthethird <ratzbodell@gmail.com>
2022-03-19 16:01:10 -0500

    Begin adding reference to AllocationToken

--- packages/hardhat/contracts/KudosToken.sol
+++ packages/hardhat/contracts/KudosToken.sol
@@ -3,14 +3,22 @@ pragma solidity ^0.7.6;
 pragma experimental ABIEncoderV2;
 
 import "hardhat/console.sol";
+import "./AllocationToken.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
+/// @title KudosToken
+/// @dev A non-transferable rewards token for FoodDAO which is issued by Kudos Guilds.
+/// A Guild contract can only mint as many Kudos tokens as its balance of AllocationToken.
+/// Many Guilds use the same KudosToken and AllocationToken, so we must track how many Kudos
+/// have been minted by a given address and store a reference to the AllocationToken contract
 contract KudosToken is Initializable, ERC20Upgradeable, PausableUpgradeable, OwnableUpgradeable {
+    AllocationToken public allocation;
 
-    function initialize(string memory name, string memory symbol) initializer public {
+    function initialize(string memory name, string memory symbol, address allocationToken) initializer public {
+        allocation = AllocationToken(allocationToken);
         __ERC20_init(name, symbol);
         __Pausable_init();
         __Ownable_init();
